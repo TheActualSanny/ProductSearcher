2ca89cd849c00c31b2feec0aaaa11a504a23fcd0
dugna1234@gmail.com <dugna1234@gmail.com>
2025-03-17 16:08:07 +0400

    feature: add pydantic classes for simple validation and transformation

---
+++ lookup/ecommerce_apis/finalized_class.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, PositiveFloat, field_validator, model_validator
+
+class Product(BaseModel):
+    '''
+        A Pydantic class which will be used for the
+        finalized products. Instead of transforming
+        some values directly while parsing,
+        this class will have validation.
+    '''
+
+    name: str
+    price: PositiveFloat
+    product_url: str
+    site: str
+
+    @field_validator('price', mode = 'before')
+    @classmethod
+    def validate_price_type(cls, value: str | float) -> float:
+        if isinstance(value, str):
+            try:
+                price = float(value[1:])
+            except ValueError:
+                seperated = value.split()
+                price = float(''.join(seperated[0][1:]))
+            return price
+        else:
+            return value
